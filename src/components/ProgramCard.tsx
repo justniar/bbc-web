7be96aa3bd448a970e9807d6b5bf3f7735720218
justniar <salsaniarno@gmail.com>
2024-09-19 15:24:55 +0700

    fix: image

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 interface ProgramCardProps {
-  src: {string};
+  src: string;
   judul: string;
   deskripsi: string;
 }
@@ -10,7 +10,7 @@ interface ProgramCardProps {
 const ProgramCard: React.FC<ProgramCardProps> = ({ src, judul, deskripsi }) => {
   return (
     <div className="w-[200px] md:w-[300px] m-[20px] md:m-[10px] p-6 md:p-8 bg-white shadow-lg cursor-pointer hover:bg-orange-50 hover:shadow-xl transition-shadow duration-300 rounded-xl flex flex-col items-center text-center space-y-3 sm:space-y-5">
-      <Image src={src} alt={judul} className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 object-contain" />
+      <Image src={src} alt={judul} className="w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 object-contain" width={100} height={100}/>
       <h2 className="text-lg sm:text-xl font-semibold text-gray-800">{judul}</h2>
       <p className="text-sm sm:text-base text-gray-600">
         {deskripsi ? deskripsi : "Lorem ipsum dolor sit amet consectetur adipisicing elit."}
