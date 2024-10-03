c4a0f0b2139b26d42a093b02be1eca7f836b64d8
justniar <salsaniarno@gmail.com>
2024-10-03 14:16:34 +0700

    add pengajar card

--- src/components/Pengajar.tsx
+++ src/components/Pengajar.tsx
@@ -1,12 +1,21 @@
 import PengajarCard from "./PengajarCard";
 
+import anna from "@/assets/pengajar/mis anna.jpeg";
+import dinah from "@/assets/pengajar/miss dinah.jpeg";
+import halimah from "@/assets/pengajar/miss halimah.jpg";
+import juju from "@/assets/pengajar/miss juju.jpeg";
+import mei from "@/assets/pengajar/miss mei.jpeg";
+import santi from "@/assets/pengajar/miss santi.jpg";
+import shally from "@/assets/pengajar/miss shally.jpeg";
+import tini from "@/assets/pengajar/miss tini.jpeg";
+
 const Pengajar = () => {
   const teamMembers = [
     {
-      name: "Miss Silvy",
+      name: "Miss Anna ",
       role: "Baca, Prisma, Bahasa Inggris",
-      description: "Mengajar merupakan skill dan passion saya",
-      imgSrc: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+      description: "Don't rely on luck, but believe in hard work",
+      imgSrc: anna,
       socialLinks: [
         {
           platform: "Facebook",
@@ -20,10 +29,112 @@ const Pengajar = () => {
       ],
     },
     {
-      name: "Miss Alma",
+      name: "Miss Dinah",
+      role: "Baca, Prisma",
+      description: "Jalani, Syukuri, Nikmati🤍",
+      imgSrc: dinah,
+      socialLinks: [
+        {
+          platform: "Twitter",
+          url: "#",
+          icon: (
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              {/* SVG content */}
+            </svg>
+          ),
+        },
+      ],
+    },
+    {
+      name: "Miss Halimah",
+      role: "Baca, Prisma",
+      description: "Life is a choice",
+      imgSrc: halimah,
+      socialLinks: [
+        {
+          platform: "Twitter",
+          url: "#",
+          icon: (
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              {/* SVG content */}
+            </svg>
+          ),
+        },
+      ],
+    },
+    {
+      name: "Miss Juju",
+      role: "Baca, Prisma",
+      description: "Tetap percaya diri diera gempuran omongan orang",
+      imgSrc: juju,
+      socialLinks: [
+        {
+          platform: "Twitter",
+          url: "#",
+          icon: (
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              {/* SVG content */}
+            </svg>
+          ),
+        },
+      ],
+    },
+    {
+      name: "Miss Mei",
       role: "Baca, Prisma",
       description: "Mengajar merupakan skill dan passion saya.",
-      imgSrc: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
+      imgSrc: mei,
+      socialLinks: [
+        {
+          platform: "Twitter",
+          url: "#",
+          icon: (
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              {/* SVG content */}
+            </svg>
+          ),
+        },
+      ],
+    },
+    {
+      name: "Miss Santi",
+      role: "Bahasa Inggris",
+      description: "Spread the love and share the happiness😃",
+      imgSrc: santi,
+      socialLinks: [
+        {
+          platform: "Twitter",
+          url: "#",
+          icon: (
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              {/* SVG content */}
+            </svg>
+          ),
+        },
+      ],
+    },
+    {
+      name: "Miss Shally",
+      role: "Baca, Prisma",
+      description: "Setiap hari adalah kesempatan baru untuk memulai",
+      imgSrc: shally,
+      socialLinks: [
+        {
+          platform: "Twitter",
+          url: "#",
+          icon: (
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              {/* SVG content */}
+            </svg>
+          ),
+        },
+      ],
+    },
+    {
+      name: "Miss Tini",
+      role: "Baca, Prisma, Bahasa Inggris",
+      description: "Jalani, Nikmati, Syukuri",
+      imgSrc: tini,
       socialLinks: [
         {
           platform: "Twitter",
--- src/components/PengajarCard.tsx
+++ src/components/PengajarCard.tsx
@@ -1,17 +1,18 @@
+import Image, { StaticImageData } from "next/image";
 interface PengajarCardProps {
   name: string;
   role: string;
   description: string;
-  imgSrc: string;
+  imgSrc: StaticImageData;
   socialLinks: { platform: string; url: string; icon: JSX.Element }[];
 }
 
 const PengajarCard = ({ name, role, description, imgSrc, socialLinks }: PengajarCardProps) => {
   return (
     <div className="flex flex-col items-center p-3 max-w-[250px] md:max-w-[500px] bg-white-500 rounded-lg shadow sm:flex-row dark:bg-gray-800 dark:border-gray-700">
-      <a href="#" className="w-full max-w-[120px] sm:max-w-[150px]">
-        <img className="w-full rounded-t-lg sm:rounded-none sm:rounded-l-lg" src={imgSrc} alt={`${name} Avatar`} />
-      </a>
+      <div className="flex justify-center items-center rrounded-t-lg sm:rounded-none sm:rounded-l-lg w-32 h-32 sm:w-40 sm:h-40 overflow-hidden ">
+        <Image className="object-cover object-center" src={imgSrc} alt={`${name} Avatar`} width={160} height={160} />
+      </div>
       <div className="p-5 flex-1 text-center sm:text-left">
         <h3 className="text-xl font-bold tracking-tight text-gray dark:text-gray">
           <a href="#">{name}</a>
